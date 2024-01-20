a8e80896dcda87277dd85f2c529bb86453d64c3e
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-01-20 17:12:26 +0530

    Validation done

--- UserService/src/libs/app/repository/authentication.repository.ts
+++ UserService/src/libs/app/repository/authentication.repository.ts
@@ -22,9 +22,7 @@ export default
         password:data.password
     }
     const response=await schema.User.create(userData)
-    console.log(response, "User created successfully");
-
-    return {status:true,message:"user creted sucessful"}
+    return {status:true,message:"user created sucessfull"}
  }
 
 }
